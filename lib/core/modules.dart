4630419aecebcc3f0146cf0afa97ec72717b1c97
Agus Sumawigena <smwgn1331@gmail.com>
2022-05-17 06:09:29 +0700

    delete controller from mxmodules

--- lib/core/modules.dart
+++ lib/core/modules.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/widgets.dart';
 
-import 'controller.dart';
-
 abstract class MxModules {
   final IconData iconData;
-  final MxController? mxController;
   final String title;
 
   MxModules({
     required this.title,
     required this.iconData,
-    this.mxController,
   });
 }
